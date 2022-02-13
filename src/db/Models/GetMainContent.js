844c29c7f1414ffd07c5a29244df88d75191b02f
Rishabh Priyadarshi <rishabh.priyadarshi@kindredgroup.com>
2022-02-13 20:13:37 +0100

    Add announcement

--- src/db/Models/GetMainContent.js
+++ src/db/Models/GetMainContent.js
@@ -1,7 +1,34 @@
-import { mainContent } from '../../data/mainContent.js';
+import { GetData, GetAllData } from '../Engines/index.js';
 
-const GetMainContent = () => {
+const GetMainContent = async () => {
+
+	const header = await GetHeaderContent();
+	const footer = await GetFooterContent();
+	const mainContent = {
+		baseUrl: "http://rishabhsahay.com",
+		header,
+		footer,
+	}
+
 	return mainContent
+}
+
+const GetHeaderContent = async () => {
+	const query = { name: 'HeaderContent' }
+	const headerData =  await GetData('MainContent', query);
+	const navigationData = await GetHeaderNavigation();
+	console.log('nav', navigationData)
+	return {...headerData, navigation: navigationData};
+
+}
+
+const GetFooterContent = async  () => {
+	const query = { name: 'FooterContent' }
+	return GetData('MainContent', query);
+}
+
+const GetHeaderNavigation = async () => {
+	return await GetAllData('HeaderNavigation')
 }
 
 export default GetMainContent
--- src/db/config.js
+++ src/db/config.js
@@ -2,3 +2,5 @@ import { MongoClient } from "mongodb";
 const uri =
   "mongodb://localhost:27017";
 export const client = new MongoClient(uri);
+client.connect();
+export const database = client.db('AAM_QL');
